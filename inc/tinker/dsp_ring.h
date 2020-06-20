f74f1ff4b562f4d600d5131c193cb4fb27efd66b
Alex Shirley <alex.shirley@egym.de>
2020-06-20 23:19:27 +0200

    Add header from spec for dsp_ring

---
+++ inc/tinker/dsp_ring.h
@@ -0,0 +1,25 @@
+#ifndef TINKER_DSP_RING_H
+#define TINKER_DSP_RING_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct dsp_ring dsp_ring_t;
+
+unsigned long dsp_ring_required_size(unsigned long element_size, unsigned long number_elements);
+bool dsp_ring_init(dsp_ring_t** __restrict c, char* block, size_t block_size, size_t element_size);
+uint32_t dsp_ring_count(dsp_ring_t* __restrict c);
+bool dsp_ring_push(dsp_ring_t* __restrict c, const void* __restrict pData);
+void dsp_ring_clear(dsp_ring_t* __restrict Fifo);
+uint32_t dsp_ring_capacity(dsp_ring_t* __restrict c);
+void * dsp_ring_index(dsp_ring_t* __restrict c, uint32_t index);
+
+#ifdef __cplusplus
+} // extern C
+#endif
+
+#endif //guard
